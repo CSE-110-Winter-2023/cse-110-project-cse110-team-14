baeb39b21580d0cca629df9d3727746b1dc9cb41
Hao Chen <hac015@ucsd.edu>
2023-02-18 22:49:46 -0800

    UI update

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import androidx.annotation.NonNull;
@@ -280,6 +281,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         ImageView imageView = findViewById(R.id.compass);
         imageView.setRotation((float) -azimuth);
+
+        if(markerList.size() > 0) {
+            //for(Marker m: markerList) {
+                Marker curMarker = markerList.get(0);
+                double markerLat = curMarker.getPosition().latitude;
+                double markerLong = curMarker.getPosition().longitude;
+                float bearingAngle = (float)calculateBearingAngle(lastLat, lastLong, markerLat, markerLong);
+                double markerDistance = calculateDistance(lastLat, lastLong, markerLat, markerLong);
+                ImageView locationIconView = findViewById(R.id.locationIcon);
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) locationIconView.getLayoutParams();
+                layoutParams.circleAngle = bearingAngle-azimuth;
+                //Toast.makeText(this, ""+lastLat+"   "+markerLat+"   "+bearingAngle, Toast.LENGTH_LONG).show();
+                locationIconView.setLayoutParams(layoutParams);
+                if(markerDistance < 30*DEFAULT_ZOOM) {
+                    locationIconView.setVisibility(View.INVISIBLE);
+                } else {
+                    locationIconView.setVisibility(View.VISIBLE);
+                }
+            //}
+        }
     }
 
     @Override
@@ -362,4 +383,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
+
+    public double calculateBearingAngle(double lat1, double long1, double lat2, double long2) {
+        double dLong = Math.toRadians(long2 - long1);
+        double lat1R = Math.toRadians(lat1);
+        double lat2R = Math.toRadians(lat2);
+        double y = Math.sin(dLong) * Math.cos(lat2R);
+        double x = Math.cos(lat1R) * Math.sin(lat2R) - Math.sin(lat1R) * Math.cos(lat2R) * Math.cos(dLong);
+        double radian = Math.atan2(y, x);
+        double bearing = Math.toDegrees(radian);
+        bearing = (bearing + 360) % 360;
+        return bearing;
+    }
+
+    public double calculateDistance(double lat1, double long1, double lat2, double long2) {
+        double R = 6378137; // Earth’s mean radius in meter
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLong = Math.toRadians(long2 - long1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        return d; // returns the distance in meter
+    };
 }
