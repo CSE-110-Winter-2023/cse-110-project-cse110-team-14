43bfac55b7a8dae4a95babc080474ebf30cebe8c
tedpark217 <jhp006@ucsd.edu>
2023-03-02 22:52:37 -0800

    Implemented ZoomObserver. Buttons are grayed out when it is not possible to zoom in or out.

--- app/src/main/java/com/example/myapplication/ZoomObserver.java
+++ app/src/main/java/com/example/myapplication/ZoomObserver.java
@@ -1,18 +1,106 @@
 package com.example.myapplication;
+
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 public class ZoomObserver {
-    private int zoomLevel = 2;
-    zoomIn(){
-        check if zoomLevel == 1;
-        if zoomLevel > 1{
-            setRings(zoomLevel - 1);
+    private TextView c1;
+    private TextView c2;
+    private TextView c3;
+    private TextView c4;
+
+    //zoom level 1 = most zoomed in
+    //zoom level 4 = most zoomed out
+    private int zoomLevel;
+    private int defaultZoom = 2;
+
+
+    public ZoomObserver(TextView c1, TextView c2, TextView c3, TextView c4){
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+        this.c4 = c4;
+        zoomLevel = defaultZoom;
+        this.setRings();
+        this.setText();
+    }
+    public void zoomIn(){
+        if(zoomLevel != 1){
+            this.setZoomLevel(this.zoomLevel-1);
+            this.setRings();
+            this.setText();
+        }
+        return;
+    }
+
+    public void zoomOut(){
+        if(zoomLevel != 4){
+            this.setZoomLevel(this.zoomLevel+1);
+            this.setRings();
+            this.setText();
+        }
+        return;
+    }
+
+    private void setRings(){
+        if(this.zoomLevel == 1){
+            this.setMargin(c4, 0, 0, 0, 0);
+            this.setMargin(c3, 0, 0, 0, 0);
+            this.setMargin(c2, 0, 0, 0, 0);
+        } else if (this.zoomLevel == 2) {
+            this.setMargin(c4, 0, 0, 0, 0);
+            this.setMargin(c3, 0, 0, 0, 0);
+            this.setMargin(c2, 250, 250, 250, 250);
+        } else if (this.zoomLevel == 3) {
+            this.setMargin(c4, 0, 0, 0, 0);
+            this.setMargin(c3, 200, 200, 200, 200);
+            this.setMargin(c2, 200, 200, 200, 200);
+        } else {
+            this.setMargin(c4, 150, 150, 150, 150);
+            this.setMargin(c3, 150, 150, 150, 150);
+            this.setMargin(c2, 150, 150, 150, 150);
         }
     }
 
-    setRings(int zoomLevel){
-        if 1:
+    private void setText(){
+        if(this.zoomLevel == 1){
+            c1.setText("\n" + "        1mi");
+            c2.setText("");
+            c3.setText("");
+            c4.setText("");
+        } else if (this.zoomLevel == 2) {
+            c1.setText("\n" + "        10mi");
+            c2.setText("  1mi");
+            c3.setText("");
+            c4.setText("");
+        } else if (this.zoomLevel == 3) {
+            c1.setText("\n" + "        500mi");
+            c2.setText("   10mi");
+            c3.setText("");
+            c4.setText("");
+        } else {
+            c1.setText("\n" + "     1000mi+");
+            c2.setText("\n" + "  500mi");
+            c3.setText("10mi");
+            c4.setText("");
+        }
+    }
 
-        if 2:
-        if 3:
-        if 4:
+    private void setMargin(TextView view, int left, int top, int right, int bottom){
+        ConstraintLayout.LayoutParams margin = (ConstraintLayout.LayoutParams) view.getLayoutParams();
+        margin.setMargins(left, top, right, bottom);
+        view.setLayoutParams(margin);
+    }
+
+    private void setZoomLevel(int level){
+        this.zoomLevel = level;
+    }
+
+    public int getZoomLevel(){
+        return this.zoomLevel;
     }
 }
--- app/src/main/java/com/example/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -1,19 +1,73 @@
 package com.example.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.ZoomObserver;
 
 public class MainActivity extends AppCompatActivity {
-    private int zoomLevel2 = 90;
-    private int zoomLevel3 = 70;
-    private int zoomLevel4 = 50;
+
+    private TextView circle1;
+    private TextView circle2;
+    private TextView circle3;
+    private TextView circle4;
+    private Button zoomIn;
+    private Button zoomOut;
+
+    private ZoomObserver zoom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setRingUI();
+    }
+
+    private void setRingUI(){
+        circle1 = findViewById(R.id.circle1);
+        circle2 = findViewById(R.id.circle2);
+        circle3 = findViewById(R.id.circle3);
+        circle4 = findViewById(R.id.circle4);
+        zoom = new ZoomObserver(circle1, circle2, circle3, circle4);
+
+        zoomIn = findViewById(R.id.zoomIn);
+        zoomOut = findViewById(R.id.zoomOut);
+    }
+
+    public void onZoomInClicked(View v) {
+        zoom.zoomIn();
+
+        if(zoom.getZoomLevel() == 1) {
+            zoomIn.setAlpha(.5f);
+            zoomIn.setClickable(false);
+        }
+        else{
+            zoomOut.setAlpha(1);
+            zoomOut.setClickable(true);
+        }
+    }
+
+    public void onZoomOutClicked(View v) {
+        zoom.zoomOut();
+        if(zoom.getZoomLevel() == 4) {
+            zoomOut.setAlpha(.5f);
+            zoomOut.setClickable(false);
+        }
+        else{
+            zoomIn.setAlpha(1);
+            zoomIn.setClickable(true);
+        }
     }
 }
