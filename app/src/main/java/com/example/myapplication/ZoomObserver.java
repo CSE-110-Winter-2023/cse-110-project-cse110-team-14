bbbbaaa9c8c65caa0a5ff3fec393abecf13a128f
tedpark217 <jhp006@ucsd.edu>
2023-03-02 17:49:07 -0800

    Pseudocode for ZoomObserver.

---
+++ app/src/main/java/com/example/myapplication/ZoomObserver.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+public class ZoomObserver {
+    private int zoomLevel = 2;
+    zoomIn(){
+        check if zoomLevel == 1;
+        if zoomLevel > 1{
+            setRings(zoomLevel - 1);
+        }
+    }
+
+    setRings(int zoomLevel){
+        if 1:
+
+        if 2:
+        if 3:
+        if 4:
+    }
+}
